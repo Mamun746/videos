153927b639b6b92f17e6eb9c91c950fdbacbc4f3
Mamun <abdullahalmamun914@gmail.com>
2019-01-04 21:26:44 +0600

    Updates

---
+++ precache-manifest.79fa882fd637a7cf55d4e51588ca2b66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb30758431df4dcb2543",
+    "url": "/videos/static/js/runtime~main.fb307584.js"
+  },
+  {
+    "revision": "8846646dd31f23ea1efb",
+    "url": "/videos/static/js/main.8846646d.chunk.js"
+  },
+  {
+    "revision": "8f241aa7d4b71d4f19f2",
+    "url": "/videos/static/js/1.8f241aa7.chunk.js"
+  },
+  {
+    "revision": "8846646dd31f23ea1efb",
+    "url": "/videos/static/css/main.259dde04.chunk.css"
+  },
+  {
+    "revision": "3ce90c50ac94c3e9831876a512812397",
+    "url": "/videos/index.html"
+  }
+];
